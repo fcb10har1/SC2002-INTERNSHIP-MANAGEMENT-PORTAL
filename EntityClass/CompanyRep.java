837fb2081287c758680e51061bd2049fb8c56718
Hareesh S <fcb10har@gmail.com>
2025-11-20 00:44:40 +0800

    Added Company Rep CLass

---
+++ EntityClass/CompanyRep.java
@@ -0,0 +1,48 @@
+package EntityClass;
+
+import java.util.List;
+
+public class CompanyRep extends User {
+    private String companyName;
+    private String department;
+    private String position;
+    private List<InternshipOpportunity> opportunities;
+    
+    // shd opportunities be here in constructor?
+    public CompanyRep(String userID, String name, String companyName, String department, String position, List<InternshipOpportunity> opportunities) {
+        super(userID, name);
+        this.companyName = companyName;
+        this.department = department;
+        this.position = position;
+        this.opportunities = opportunities;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public List<InternshipOpportunity> getOpportunities() {
+        return opportunities;
+    }
+    
+}
