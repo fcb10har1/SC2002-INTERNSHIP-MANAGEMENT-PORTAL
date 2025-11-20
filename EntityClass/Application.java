5a6276480c5f20ecd6f014c9a0821787684b03a9
Hareesh S <fcb10har@gmail.com>
2025-11-20 16:08:04 +0800

    Edited Application Class :)

--- EntityClass/Application.java
+++ EntityClass/Application.java
@@ -1,11 +1,17 @@
 package EntityClass;
 
+import EntityClass.Enums.*;
+
 public class Application {
-    Student student;
-    InternshipOpportunity target;
-    ApplicationStatus status = ApplicationStatus.PENDING;
-    boolean acceptedByStudent = false;
-}
+    private Student student;
+    private InternshipOpportunity target;
+    private ApplicationStatus status = ApplicationStatus.Pending;
+    private boolean acceptedByStudent = false;
+
+    public Application(Student student, InternshipOpportunity target) {
+        this.student = student;
+        this.target = target;
+    }
 
     public ApplicationStatus getStatus() {
         return status;
@@ -19,6 +25,24 @@ public class Application {
         return acceptedByStudent;
     }
 
+    public Student getStudent() {
+        return student;
+    }
+
+    public InternshipOpportunity getTarget() {
+        return target;
+    }
+
+    public boolean isconfirmed() {
+        return this.status == ApplicationStatus.Successful && this.acceptedByStudent;
+    }
+
+    public void studentAccept() {
+        this.acceptedByStudent = true;
+    }
+    
+
+}
     
 
 
